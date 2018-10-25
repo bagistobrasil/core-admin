73437b05f89858bf5cc7fa986e13081848c411ed
rahul shukla <rahulshukla.symfony517@webkul.com>
2018-10-25 13:37:21 +0530

    error pages

--- src/Resources/views/errors/401.blade.php
+++ src/Resources/views/errors/401.blade.php
@@ -1,10 +1,11 @@
-@extends('admin::layouts.content')
+@extends('shop::layouts.master')
 
 @section('page_title')
-    {{ __('admin::app.error.403.page-title') }}
+    {{ __('admin::app.error.401.page-title') }}
 @stop
 
-@section('content')
+@section('content-wrapper')
+
     <div class="error-container" style="width: 100%; display: flex; justify-content: center;">
 
         <div class="wrapper" style="display: flex; height: 60vh; width: 100%;
@@ -12,19 +13,17 @@
 
             <div class="error-box"  style="width: 50%">
 
-                <div class="error-title" style="font-size: 100px;color: #5E5E5E">
-                    {{ __('admin::app.error.403.name') }}
-                </div>
+                <div class="error-title" style="font-size: 100px;color: #5E5E5E">  {{ __('admin::app.error.401.name') }}  </div>
 
                 <div class="error-messgae" style="font-size: 24px;color: #5E5E5E">
-                    {{ __('admin::app.error.403.title') }}
+                    {{ __('admin::app.error.401.title') }}
                 </div>
 
                 <div class="error-description" style="margin-top: 20px;margin-bottom: 20px;color: #242424">
-                    {{ __('admin::app.error.403.message') }}
+                    {{ __('admin::app.error.401.message') }}
                 </div>
 
-                <a href="{{ route('admin.dashboard.index') }}">
+                <a href="{{ route('shop.home.index') }}">
                     {{ __('admin::app.error.go-to-home') }}
                 </a>
 
@@ -35,4 +34,5 @@
         </div>
 
     </div>
-@stop
+
+@endsection
